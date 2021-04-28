134075f6c8a5e0c185bb65f767ad33facf2dcf32
Sycophantam <61561014+Sycophantam@users.noreply.github.com>
2021-04-28 12:51:38 -0700

    Using command line arguments instead of hardcoding file names.

--- main.cpp
+++ main.cpp
@@ -7,15 +7,12 @@
 
 using namespace std;
 
-int main()
+int main(int arcg, char* argv[])
 {
-    //Encoding a text file of The Great Gatsby, by F. Scott Fitzgerald
-    //Text file provided by Project Gutenberg Australia
-    //Link: http://gutenberg.net.au/ebooks02/0200041.txt
-    HuffmanTree tree("gatsby.txt");
-//    tree.printBits();
-//    tree.printFreqs();
+    string fname = argv[1];
+    string noExtension = fname.substr(0, fname.find('.'));
+    HuffmanTree tree(fname);
     tree.toFile();
-    tree.fromFile("gatsby_encoded.bin");
+    tree.fromFile(noExtension + "_encoded.bin");
     return 0;
 }
